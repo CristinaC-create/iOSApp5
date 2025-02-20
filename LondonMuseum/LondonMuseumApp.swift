110b1f1c0a12c0c645b229a6881b637f4379d102
CristinaC-Create <Cristina.Cabral@triosstudent.com>
2025-02-19 21:00:06 -0500

    Initial Commit

---
+++ LondonMuseum/LondonMuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LondonMuseumApp.swift
+//  LondonMuseum
+//
+//  Created by Cristina Cabral on 2025-02-19.
+//
+
+import SwiftUI
+
+@main
+struct LondonMuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
