5850c4f1b91cb0940757d835bbfc27b0f52a13fc
Cristina <Cristina.Cabral@triosstudent.com>
2025-03-03 20:51:55 -0500

    Updated App

---
+++ LondonMuseum/Artwork.swift
@@ -0,0 +1,8 @@
+//
+//  Artwork.swift
+//  iOSApp5
+//
+//  Created by Cristina Cabral on 2025-02-27.
+//
+
+import Foundation
---
+++ LondonMuseum/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  iOSApp5
+//
+//  Created by Cristina Cabral on 2025-02-27.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryView()
+}
